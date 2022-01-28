b5fb749535108ef44b6a377d8aebe53f54a075a8
poblete <pablo.poblete@ing.uchile.cl>
2022-01-27 22:30:20 -0300

    se agrega sdk

--- deploy.js
+++ deploy.js
@@ -4,16 +4,16 @@ const Promise = require('bluebird');
 const lambda = new AWS.Lambda({
   region: 'us-west-2'
 });
-var lambdaName = process.argv[2]; //value will be "banana"
+var lambdaName = process.argv[2]; 
 const cwd = process.cwd();
 const zipLambdaCommand = `
-  cd ${cwd}/lambda/${lambdaName}/ &&
+  cd ${cwd}/lambda_files/${lambdaName}/ &&
   npm install --production &&
   zip -r ${lambdaName}.zip * --quiet`;
 
 const lambdaUpdateFunctionCodeParams = {
   FunctionName: `${lambdaName}`,
   Publish: true,
-  ZipFile: read(`${cwd}/lambda/${lambdaName}/${lambdaName}.zip`)
+  ZipFile: read(`${cwd}/lambda_files/${lambdaName}/${lambdaName}.zip`)
  };
 lambdaUpdateFunctionCode(lambdaUpdateFunctionCodeParams);
